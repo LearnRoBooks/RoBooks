d4dc989539466a7969d37eb15d302d3afaf9c8c8
OmarAttaalla <74081885+OmarAttaalla@users.noreply.github.com>
2021-09-20 20:33:34 -0700

    make it better

--- Index.js
+++ Index.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose')
+const Receipts = require('../src/models/Receipts')
+require('dotenv/config')
 
 const app = express();
 const cookieParser = require('cookie-parser')
+const BodyParser = require('body-parser')
 const TemplatePath = path.join(__dirname, 'public', 'template.html');
 const LoginPage = path.join(__dirname, 'public', 'loginPage.html');
 
 
 //GOOGLE AUTHENTICATION
 const {OAuth2Client} = require('google-auth-library');
+const { config } = require('dotenv');
 const CLIENT_ID = '1030385101120-oud1pj9gnqed70prr548rslnr4j8a4us.apps.googleusercontent.com'
 const client = new OAuth2Client(CLIENT_ID);
 
@@ -16,6 +21,7 @@ const client = new OAuth2Client(CLIENT_ID);
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.json());
 app.use(cookieParser());
+app.use(BodyParser.json());
 
 app.get('/', checkAuthenticated, (Request,Response) => {
     Response.redirect('/profile');
@@ -48,11 +54,57 @@ app.post('/login', (Request,Response) => {
     catch(console.error);
 })
 
-app.get('/profile', checkAuthenticated, (Request,Response) => {
+app.get('/profile', checkAuthenticated, async (Request,Response) => {
     let user = Request.user;
+
+    console.log(user)
+
+    try{
+        const ReceiptsList = await Receipts.findById(user.email);
+        if (ReceiptsList == null) {
+            const Receiptnew = new Receipts({
+                _id: Request.user.email,
+                holdata: []
+            });
+
+            console.log(Receiptnew)
+
+            Receiptnew.save()
+                .then(data => {
+                    Response.json(data);
+                })
+                .catch(err => {
+                    Response.json({message: err})
+                })
+        }
+        console.log(ReceiptsList)
+    
+    }catch(err){
+        console.log(err)
+    }
+    
 
     
     Response.sendFile(TemplatePath)
+})
+
+app.get('/getData', checkAuthenticated, async (Request,Response) => {
+    let user = Request.user;
+
+    console.log("getting data")
+
+    try{
+        const ReceiptsList = await Receipts.findById(user.email);
+        
+        console.log(ReceiptsList)
+        console.log('Did Get Data')
+
+        Response.json(ReceiptsList)
+        
+    }catch(err){
+        console.log(err)
+        Response.send(err)
+    }
 })
 
 app.get('/logout', (Request,Response) =>{
@@ -60,9 +112,34 @@ app.get('/logout', (Request,Response) =>{
     Response.redirect('/login')
 })
 
+//CONNECT TO DATABASE
+mongoose.connect(process.env.DB_CONNECTION, () => {console.log('Connected to DB')})
 
+app.post('/profile', checkAuthenticated, async (req, res) => {
 
+    console.log('Request')
 
+    try {
+        const dataToUpdate = {
+            date: req.body.date,
+            place: req.body.place,
+            amount: req.body.amount,
+            payment: req.body.payment
+        }
+
+        console.log(req)
+        console.log(req.body)
+
+        const updatedReceipt = await Receipts.updateOne({_id: req.user.email}, { $push: { holdata: dataToUpdate}})
+        console.log('Updated Receipt')
+        console.log(updatedReceipt)
+        res.json(updatedReceipt)
+    } catch (err) {
+        console.log(err)
+        res.json({message: err})
+    }
+    
+})
 
 
 //COOKIE AUTHENTICATION 
@@ -91,6 +168,9 @@ function checkAuthenticated(req, res, next){
       })
 
 }
+
+
+
 
 const Port = process.env.PORT || 3000;
 
